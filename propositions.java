35b0ce8303b9fc379b63478b1765d037c6bdc300
angelcamach0 <54860242+angelcamach0@users.noreply.github.com>
2019-09-23 23:33:40 -0600

    Update propositions.java

--- propositions.java
+++ propositions.java
@@ -1,259 +1,195 @@
 //Angel Camacho
 //CS 278
-//Lab 2
-//Logical Connectives, Tautologies, and Contradictions
-public class propositions {
-
-	//public static void main(String[] args) {
-		
+//Lab 3
+//Quantified Statements
+package lab3;
+import java.util.Scanner;
+public class quantifieStatements {
 	
-	//part 1
-		//negation method ~
-		public static char negation(char P) {
-			char R;
-			if ( P == 'T' ) {
-				R = 'F';
-			}else if(P == 'F') {
-				R = 'T';
-			}else {
-				R = 'T';
-			}
-			return R;
-		}
-		
-		//disjunctive OR
-		public static char disjunctive(char P, char Q) {
-			char R;
-
-			if(P == 'F' && Q == 'F') {
-				R = 'F';
-			}else {
-				R = 'T';
-			}
-			return R;
-
-		}
-		//conjunctive AND
-		public static char conjunctive(char P, char Q) {
-			char R;
-			if(P== 'T' && Q == 'T') {
-				R = 'T';
-			}else {
-				R = 'F';
-			}
-			return R;
-		}
-		//implicative ->
-		public static char implicative(char P, char Q) {
-			char R;
-			if(P == 'F') {
-				R = 'T';
-			}else if((P == 'T' && Q == 'F')) {
-				R = 'F';
-			}else{
-				R = 'T';
-			}
-			return R;
-		}
-		
-		//Part 2
-		public static void main(String args[]) {
-		System.out.println("");
-		System.out.println("All possible combinations for proposition 1");
-		System.out.println("P Q R  Truth Value");
-		System.out.println("-------------------");
-		char P;
-		char Q;
+	static int[]mainArray = new int [10];
+	
+	
+//	public static String All(int a[]) {
+//		String temp = "is False";
+//		
+//		for(int i = 0; i < a.length; i++) {
+//			
+//		}
+//		return temp;
+//	}
+	
+	//conjunctive AND
+	public static char conjunctive(char P, char Q) {
 		char R;
-		int propCounter = 0;
-		int truthCounter =0;
-
-		//proposition 1
-		for(int Pline = 0; Pline < 2; Pline++) {
-			for(int Qline = 0; Qline < 2; Qline++) {
-				for(int Rline = 0; Rline < 2; Rline++) {
-					
-					if(Pline == 0) {
-						P = 'T';
-					}else {
-						P = 'F';
-					}
-					if(Qline == 0) {
-						Q = 'T';
-					}else {
-						Q = 'F';
-					}
-					if(Rline == 0) {
-						R = 'T';
-					}else {
-						R = 'F';
-					}
+		if(P== 'T' && Q == 'T') {
+			R = 'T';
+		}else {
+			R = 'F';
+		}
+		return R;		
+	}
 	
-					System.out.println(P + " " + Q + " " + R + "      " + conjunctive((implicative(negation(P), Q)), (implicative(R,P))));
-					if( conjunctive((implicative(negation(P), Q)), (implicative(R,P))) == 'T') {
-						propCounter++;
-					}
-					truthCounter++;
+	//All
+	public static String All(char a[]) {
+		String temp = "is False";
+		int count = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			if( a[i] == 'T') {
+				count ++;
+			}
+		}
+		if(count == a.length) {
+			return "Is True";
+		}
+		return temp;
+	}
+	
+	
+	//graterThanZero method for array
+	public static []char graterThanZero(int a[]) {
+		String returnVal = "True";
+		bubbleSort(a);
+		int howManyTrue = 0;
+		int howManyFalse = 0;
+		char caseTrue = 'T';
+		
+		//idea
+		char []truthValOfArray = new char [10];
+		
+		//Quantifing number of truths and number of false's in array
+		for(int i = 0; i < a.length; i++) {
+			if(a[i] > 0) {
+				//this will insert the truth value in the array.
+				truthValOfArray[i] = 'T';
+				howManyTrue++;
+			}
+			if(a[i]<0) {
+				//this will insert the truthValue in the array
+				truthValOfArray[i] = 'F';
+				howManyFalse++;
+			}
+			
+		}
+		
+//		//my return values
+//		if(howManyTrue == a.length) {
+//			return caseTrue;
+//		}
+		
+		return truthValOfArray[];
+	}
+	
+//	//this case is for ALL but im still not sure if it applys to others.
+//	public static char graterThanZero(int a) {
+//		String returnVal = "True";
+//		int howManyTrue = 0;
+//		int howManyFalse = 0;
+//		char caseTrue = 'T';
+//		
+//		//Quantifing number of truths and number of false's in array
+//			if(a > 0) {
+//				howManyTrue++;
+//			}
+//			
+//		//my return values
+//			if(howManyTrue > 0) {
+//				return caseTrue;
+//			}
+//			return 'F';
+//	}
+	
+	
+	public static char isItEven(int a[]) {
+		bubbleSort(a);
+		char caseTrue = 'T';
+		
+		int howManyTrue = 0;
+		for(int i = 0; i < a.length; i++) {
+			if((a[i] % 2) == 0) {
+				howManyTrue++;
+			}
+		}
+		if(howManyTrue == a.length) {
+			return caseTrue;
+		}
+		return 'F';
+	}
+	
+//	public static char isItEven(int a) {
+//		char caseTrue = 'T';
+//		int howManyTrue = 0;
+//			if((a % 2) == 0) {
+//				howManyTrue++;
+//			}
+//			if(howManyTrue > 0) {
+//				return caseTrue;
+//			}
+//		return 'F';
+//	}
+	
+	
+	
+	public static void bubbleSort(int a[]) {
+		int length = a.length;
+		int temp3 = 0;
+		for(int k = 0; k < length; k++) {
+			for(int l = 1; l < (length - k); l++) {
+				if(a[l-1]>a[l]) {
+					temp3 = a[l-1];
+					a[l-1] = a[l];
+					a[l] = temp3;
 				}
 			}
 		}
-		if(propCounter==truthCounter) {
-			System.out.print("Tutology!");
-			System.out.println("");
-
-		}else if(propCounter==0){
-			System.out.print("Contradiction!");
-			System.out.println("");
-
-		}else {
-			System.out.print("Neither!");
-			System.out.println("");
-
-		}
-
-		//proposition 2
-		propCounter = 0;
-		truthCounter = 0;
-		System.out.println("");
-		System.out.println("All possible combinations for proposition 2");
-		System.out.println("P Q R  Truth Value");
-		System.out.println("-------------------");
-		for(int Pline = 0; Pline < 2; Pline++) {
-			for(int Qline = 0; Qline < 2; Qline++) {
-				for(int Rline = 0; Rline < 2; Rline++) {
-					
-					if(Pline == 0) {
-						P = 'T';
-					}else {
-						P = 'F';
-					}
-					if(Qline == 0) {
-						Q = 'T';
-					}else {
-						Q = 'F';
-					}
-					if(Rline == 0) {
-						R = 'T';
-					}else {
-						R = 'F';
-					}
+	}
 	
-					System.out.println(P + " " + Q + " " + R + "      " + conjunctive(disjunctive(P,negation(Q)),disjunctive(R,negation(implicative(P,Q)))));
-					if( conjunctive(disjunctive(P,negation(Q)),disjunctive(R,negation(implicative(P,Q)))) == 'T') {
-						propCounter++;
-					}
-					truthCounter++;
-				}
-			}
-		}
-		if(propCounter==truthCounter) {
-			System.out.print("Tutology!");
-			System.out.println("");
-
-		}else if(propCounter==0){
-			System.out.print("Contradiction!");
-			System.out.println("");
-
-		}else {
-			System.out.print("Neither!");
-			System.out.println("");
-
-		}
-		
-		//proposition 3
-		propCounter = 0;
-		truthCounter = 0;
-		System.out.println("");
-		System.out.println("All possible combinations for proposition 3");
-		System.out.println("P Q    Truth Value");
-		System.out.println("-------------------");
-		for(int Pline = 0; Pline < 2; Pline++) {
-			for(int Qline = 0; Qline < 2; Qline++) {
-					
-					if(Pline == 0) {
-						P = 'T';
-					}else {
-						P = 'F';
-					}
-					if(Qline == 0) {
-						Q = 'T';
-					}else {
-						Q = 'F';
-					}
-
-					System.out.println(P + " " + Q + " " + " " + "      " + implicative(P,implicative(negation(conjunctive(P,negation(Q))), conjunctive(P,Q))) );
-					if( implicative(P,implicative(negation(conjunctive(P,negation(Q))), conjunctive(P,Q))) == 'T') {
-						propCounter++;
-					}
-					truthCounter++;
-
-			}
-		}
-		if(propCounter==truthCounter) {
-			System.out.print("Tutology!");
-			System.out.println("");
-
-		}else if(propCounter==0){
-			System.out.print("Contradiction!");
-			System.out.println("");
-
-		}else {
-			System.out.print("Neither!");
-			System.out.println("");
-
-		}
-
-		//proposition 4
-		propCounter = 0;
-		truthCounter = 0;
-		System.out.println("");
-		System.out.println("All possible combinations for proposition 4");
-		System.out.println("P Q    Truth Value");
-		System.out.println("-------------------");
-		for(int Pline = 0; Pline < 2; Pline++) {
-			for(int Qline = 0; Qline < 2; Qline++) {
-				
-					if(Pline == 0) {
-						P = 'T';
-					}else {
-						P = 'F';
-					}
-					if(Qline == 0) {
-						Q = 'T';
-					}else {
-						Q = 'F';
-					}
-
-					System.out.println(P + " " + Q + " " + " " + "      " + conjunctive(conjunctive(P,(implicative(P,Q))),negation(Q)) );
-					if( conjunctive(conjunctive(P,(implicative(P,Q))),negation(Q)) == 'T') {
-						propCounter++;
-					}
-					truthCounter++;
-			}
-		}
-		if(propCounter==truthCounter) {
-			System.out.print("Tutology!");
-			System.out.println("");
-
-		}else if(propCounter==0){
-			System.out.print("Contradiction!");
-			System.out.println("");
-
-		}else {
-			System.out.print("Neither!");
-			System.out.println("");
-
-		}
-
-		
 	
+	public static void main(String[] args) {
 		
+		//int []d = new int [10];
+		Scanner scan = new Scanner(System.in);
+		int counter = 9;
+		
+		System.out.println("Please input 10 integers. (With no repeated entries)");
+		for(int i = 0; i < 10; i++) {
+			mainArray[i] = scan.nextInt();
+			if(counter > 0) {
+				System.out.println("Please input another value.");
+				counter--;
+			}
 		}
 
+		System.out.println("This is your counter: "+counter);
+		System.out.print("This is in your array: ");
+		for(int p = 0; p < 10; p++) {
+				System.out.print(mainArray[p] + " ");
+		}
+		System.out.println("");
+		System.out.print("This is your sorted array: ");
+		bubbleSort(mainArray);
+		for(int p = 0; p < 10; p++) {
+			System.out.print(mainArray[p] + " ");
+		}
+		
+		System.out.println("");
+		//System.out.println("Is it grater thatn zero?: "+ graterThanZero(mainArray));
+		
+		System.out.println("");
+		//System.out.println("Is it even?: "+ isItEven(mainArray));
 		
 		
+		//Statement a
 		
+		//Statement b
 		
+		//Statement c
 		
-
+		//Statement d
+		
+	}
+	
+	
+	
+	
 }
